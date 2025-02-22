567786605e440389756bb6581f107ba7c1127d78
Bruno Albuquerque Vaz Jucoski <141188686+brunojucoski@users.noreply.github.com>
2025-02-22 00:39:08 +0000

    aulinha bem diboa

---
+++ script.js
@@ -0,0 +1,45 @@
+const divConteudo = document.querySelector("#conteudo");
+
+divConteudo.innerHTML = "manipulado via JS";
+
+const ulListaDestino =document.createElement("ul");
+const liNewYork = document.createElement("li");
+
+
+liNewYork.textContent = "Nova Iorque";
+
+ulListaDestino.appendChild(liNewYork);
+divConteudo.appendChild(ulListaDestino);
+
+// let idade;
+
+// do {
+//    idade = prompt ("informe sua idade")
+
+//    if(idade <18){
+// alert("Você não pode ter CNH");
+//    }else{
+//     alert("Você pode ter CNH")
+//    }
+
+// } while(idade < 18)
+function entrar() {
+
+    const nome = document.getElementById("nome").value;
+    
+    if (nome && nome !== '') {
+        divConteudo.innerHTML = "Bem vindo, " + nome; 
+       // document.getElementById("mensagem").innerText = `Bem-vindo, ${nome}!`;
+    } else {  
+        divConteudo.innerHTML = "informe o seu nome" ; 
+        //document.getElementById("mensagem").innerText = "Por favor, insira um nome.";
+    }
+}
+
+const ulfor =document.createElement("ul");
+for(let i=0; i <=10 ;i ++) {
+     const lifor = document.createElement("li");
+     lifor.textContent = "numero: " + i ;
+     divConteudo.appendChild(lifor); 
+}
+divConteudo.appendChild(ulfor); 
